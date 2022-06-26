417f8471733d36bc3247abab8c4a39d0e3b0f1ab
queirozdaniel <danielqueirozak77@gmail.com>
2022-06-25 22:58:32 -0300

    chore: Initial SpringBoot Project

---
+++ src/main/kotlin/com/danielqueiroz/libraryshop/LibraryShopApplication.kt
@@ -0,0 +1,11 @@
+package com.danielqueiroz.libraryshop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LibraryShopApplication
+
+fun main(args: Array<String>) {
+    runApplication<LibraryShopApplication>(*args)
+}
---
+++ src/test/kotlin/com/danielqueiroz/libraryshop/LibraryShopApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.danielqueiroz.libraryshop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LibraryShopApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
