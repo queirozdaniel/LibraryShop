3d1e91b181e5c474820f945c7b20713c26cf0f74
queirozdaniel <danielqueirozak77@gmail.com>
2022-06-27 22:38:50 -0300

    refactor(PersonController): Using util.MediaType to provide static information

---
+++ src/main/kotlin/com/danielqueiroz/libraryshop/api/config/util/MediaType.kt
@@ -0,0 +1,7 @@
+package com.danielqueiroz.libraryshop.api.config.util
+
+object MediaType {
+    const val APPLICATION_JSON = "application/json"
+    const val APPLICATION_XML = "application/xml"
+    const val APPLICATION_YAML = "application/x-yaml"
+}
--- src/main/kotlin/com/danielqueiroz/libraryshop/api/controller/PersonController.kt
+++ src/main/kotlin/com/danielqueiroz/libraryshop/api/controller/PersonController.kt
@@ -1,9 +1,9 @@
 package com.danielqueiroz.libraryshop.api.controller
 
+import com.danielqueiroz.libraryshop.api.config.util.MediaType
 import com.danielqueiroz.libraryshop.api.data.vo.v1.PersonVO
 import com.danielqueiroz.libraryshop.api.data.vo.v2.PersonVO as PersonVOV2
 import com.danielqueiroz.libraryshop.domain.service.PersonService
-import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -13,27 +13,27 @@ class PersonController(
     private val personService: PersonService
 ) {
 
-    @GetMapping("/{id}", produces = [MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE, "application/x-yaml"])
+    @GetMapping("/{id}", produces = [MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML])
     fun getPerson(@PathVariable id: Long): PersonVO {
         return personService.findById(id)
     }
 
-    @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE])
+    @GetMapping(produces = [MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML])
     fun getPersons(): List<PersonVO> {
         return personService.findAll()
     }
 
-    @PostMapping(produces = [MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE], consumes = [MediaType.APPLICATION_JSON_VALUE])
+    @PostMapping(produces = [MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML,MediaType.APPLICATION_YAML], consumes = [MediaType.APPLICATION_JSON])
     fun createPerson(@RequestBody person: PersonVO): PersonVO {
         return personService.create(person)
     }
 
-    @PostMapping("/v2",produces = [MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE], consumes = [MediaType.APPLICATION_JSON_VALUE])
+    @PostMapping("/v2",produces = [MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML], consumes = [MediaType.APPLICATION_JSON])
     fun createPersonV2(@RequestBody person: PersonVOV2): PersonVOV2 {
         return personService.createV2(person)
     }
 
-    @PutMapping(produces = [MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE], consumes = [MediaType.APPLICATION_JSON_VALUE])
+    @PutMapping(produces = [MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML], consumes = [MediaType.APPLICATION_JSON])
     fun updatePerson(@RequestBody person: PersonVO): PersonVO {
         return personService.update(person)
     }
