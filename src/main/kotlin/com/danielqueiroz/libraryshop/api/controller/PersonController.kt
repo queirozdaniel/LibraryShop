d3847f7cf7ccbae7c7e322f5807f96d61d080742
queirozdaniel <danielqueirozak77@gmail.com>
2022-06-30 00:28:48 -0300

    refactor(PersonController!): Change endpoints to config OpenApi

--- src/main/kotlin/com/danielqueiroz/libraryshop/api/controller/PersonController.kt
+++ src/main/kotlin/com/danielqueiroz/libraryshop/api/controller/PersonController.kt
@@ -8,22 +8,22 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/persons")
+@RequestMapping("/api/persons/")
 class PersonController(
     private val personService: PersonService
 ) {
 
-    @GetMapping("/{id}", produces = [MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML])
+    @GetMapping("/v1/{id}", produces = [MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML])
     fun getPerson(@PathVariable id: Long): PersonVO {
         return personService.findById(id)
     }
 
-    @GetMapping(produces = [MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML])
+    @GetMapping("/v1",produces = [MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML])
     fun getPersons(): List<PersonVO> {
         return personService.findAll()
     }
 
-    @PostMapping(produces = [MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML,MediaType.APPLICATION_YAML], consumes = [MediaType.APPLICATION_JSON])
+    @PostMapping("/v1",produces = [MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML,MediaType.APPLICATION_YAML], consumes = [MediaType.APPLICATION_JSON])
     fun createPerson(@RequestBody person: PersonVO): PersonVO {
         return personService.create(person)
     }
@@ -33,12 +33,13 @@ class PersonController(
         return personService.createV2(person)
     }
 
-    @PutMapping(produces = [MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML], consumes = [MediaType.APPLICATION_JSON])
-    fun updatePerson(@RequestBody person: PersonVO): PersonVO {
+    @PutMapping("/v1/{id}",produces = [MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML], consumes = [MediaType.APPLICATION_JSON])
+    fun updatePerson(@RequestBody person: PersonVO, @PathVariable id: Long): PersonVO {
+        person.idValue = id
         return personService.update(person)
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/v1/{id}")
     fun deletePerson(@PathVariable id: Long): ResponseEntity<*> {
         personService.delete(id)
         return ResponseEntity.noContent().build<Any>()
