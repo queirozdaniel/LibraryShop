024b0a42f3783540be10090cc144e53457946051
queirozdaniel <danielqueirozak77@gmail.com>
2022-06-29 23:30:29 -0300

    fix: Json Response

--- src/main/kotlin/com/danielqueiroz/libraryshop/api/data/vo/v1/PersonVO.kt
+++ src/main/kotlin/com/danielqueiroz/libraryshop/api/data/vo/v1/PersonVO.kt
@@ -1,10 +1,14 @@
 package com.danielqueiroz.libraryshop.api.data.vo.v1
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
 import com.github.dozermapper.core.Mapping
 import org.springframework.hateoas.RepresentationModel
 
+@JsonPropertyOrder("id", "firstName", "lastName", "address", "gender")
 data class PersonVO(
     @Mapping("id")
+    @field:JsonProperty("id")
     var idValue: Long? = null,
     var firstName: String = "",
     var lastName: String = "",
