5fd9084c9d79ed74e3d1ccb30a7161a9578e7e11
queirozdaniel <danielqueirozak77@gmail.com>
2022-07-03 18:38:21 -0300

    feat: Configuration CORS in application

--- src/main/kotlin/com/danielqueiroz/libraryshop/api/config/WebConfig.kt
+++ src/main/kotlin/com/danielqueiroz/libraryshop/api/config/WebConfig.kt
@@ -1,14 +1,19 @@
 package com.danielqueiroz.libraryshop.api.config
 
 import com.danielqueiroz.libraryshop.api.config.serialization.converter.YamlJackson2HttpMessageConverter
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.MediaType
 import org.springframework.http.converter.HttpMessageConverter
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig : WebMvcConfigurer {
+
+    @Value("\${cors.originPatterns:default}")
+    private val corsOriginPatterns: String = ""
 
     private val MEDIA_TYPE_APPLICATION_YMAL = MediaType.valueOf("application/x-yaml")
 
@@ -24,6 +29,14 @@ class WebConfig : WebMvcConfigurer {
                 .mediaType("json", MediaType.APPLICATION_JSON)
                 .mediaType("xml", MediaType.APPLICATION_XML)
                 .mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YMAL)
+    }
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        val allowedOrigins = corsOriginPatterns.split(",").toTypedArray()
+        registry.addMapping("/**")
+            .allowedMethods("*")
+            .allowedOrigins(*allowedOrigins)
+            .allowCredentials(true)
     }
 
 }
---
+++ src/main/kotlin/com/danielqueiroz/libraryshop/api/config/WebServerConfiguration.kt
@@ -0,0 +1,24 @@
+package com.danielqueiroz.libraryshop.api.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebServerConfiguration {
+
+    @Value("\${cors.originPatterns:default}")
+    private val corsOriginPatterns: String = ""
+
+    @Bean
+    fun addCorsConfig(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                super.addCorsMappings(registry)
+            }
+        }
+    }
+
+}
