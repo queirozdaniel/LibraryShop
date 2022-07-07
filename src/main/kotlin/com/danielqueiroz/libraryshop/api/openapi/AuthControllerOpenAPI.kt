af6e22a5b2d2db5c45a997cf1cdb3772c8fb73ea
queirozdaniel <danielqueirozak77@gmail.com>
2022-07-06 23:59:13 -0300

    docs: Added description for refresh token endpoint

--- src/main/kotlin/com/danielqueiroz/libraryshop/api/openapi/AuthControllerOpenAPI.kt
+++ src/main/kotlin/com/danielqueiroz/libraryshop/api/openapi/AuthControllerOpenAPI.kt
@@ -4,12 +4,20 @@ import com.danielqueiroz.libraryshop.api.data.vo.v1.AccountCredentialsVO
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 
 @Tag(name = "Authentication Endpoint")
 interface AuthControllerOpenAPI {
 
     @Operation(summary = "Authenticates an user and return a token")
     fun signin(@RequestBody data: AccountCredentialsVO): ResponseEntity<*>
+
+    @Operation(summary = "Refresh token for authenticated user and returns a token")
+    fun refreshToken(
+        @PathVariable username: String,
+        @RequestHeader("Authorization") refreshToken: String
+    ): ResponseEntity<*>
 
 }
