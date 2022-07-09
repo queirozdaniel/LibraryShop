91e5bd9bf14060ab588124008f7adef01a52770d
queirozdaniel <danielqueirozak77@gmail.com>
2022-07-08 23:46:16 -0300

    feat: Capture error when credentials are invalids

--- src/main/kotlin/com/danielqueiroz/libraryshop/api/exception/handler/CustomizedResponseEntityExceptionHandler.kt
+++ src/main/kotlin/com/danielqueiroz/libraryshop/api/exception/handler/CustomizedResponseEntityExceptionHandler.kt
@@ -6,6 +6,7 @@ import com.danielqueiroz.libraryshop.api.exception.RequiredObjectIsNullException
 import com.danielqueiroz.libraryshop.api.exception.ResourceNotFoundException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.context.request.WebRequest
@@ -36,8 +37,16 @@ class CustomizedResponseEntityExceptionHandler {
             HttpStatus.BAD_REQUEST
         )
     }
+
     @ExceptionHandler(InvalidJwtAuthenticationException::class)
     fun handleInvalidJwtAuthenticationException(ex: Exception, request: WebRequest): ResponseEntity<ExceptionResponse> {
+        return ResponseEntity<ExceptionResponse>(
+            ExceptionResponse(LocalDateTime.now(), ex.message, request.getDescription(false)),
+            HttpStatus.FORBIDDEN
+        )
+    }
+    @ExceptionHandler(BadCredentialsException::class)
+    fun handleInvalidJwtAuthenticationCredentialsException(ex: Exception, request: WebRequest): ResponseEntity<ExceptionResponse> {
         return ResponseEntity<ExceptionResponse>(
             ExceptionResponse(LocalDateTime.now(), ex.message, request.getDescription(false)),
             HttpStatus.FORBIDDEN
